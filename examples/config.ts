2be892f62c4efe953da8977714242ec27f5d88e2
oddy <mblancsans@gmail.com>
2026-04-11 17:25:31 +0200

    cleanned examples

--- examples/config.ts
+++ examples/config.ts
@@ -3,14 +3,14 @@ import { getConfig, setConfig } from "@gamedev.upc/vilahack-sdk";
 // Set API endpoint
 const url: string = "API_URL"; // Test, Prod, etc.
 
-async function getSupabaseToken(): Promise<string | null> {
+async function getToken(): Promise<string | null> {
   return "API_TOKEN"; // Supabase token.
 }
 
 // Set config
 setConfig({
   baseUrl: url,
-  getToken: getSupabaseToken,
+  getToken: getToken,
 });
 
 // Example for verification
